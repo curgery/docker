347371915875f4670928cb9703a567a232dbc496
curgery <androidgeek54@gmail.com>
2021-08-01 17:01:19 -0400

    test commit for permissions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>RLG Mobile Mobile Applications -Travis CI/AWS</p>
+        <p>RLG Mobile Applications -Travis CI/AWS</p>
         <a
           className='App-link'
           href='https://reactjs.org'
