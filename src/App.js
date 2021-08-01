b59b48df90fa0e3fe373b985a807ae1cd72febc8
curgery <androidgeek54@gmail.com>
2021-08-01 17:35:39 -0400

    testing permissions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>RLG Mobile Development-Travis CI/AWS</p>
+        <p>RLG Enterprises LLC-Travis CI/AWS</p>
         <a
           className='App-link'
           href='https://reactjs.org'
